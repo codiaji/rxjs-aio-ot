cf025e9bf2c93f47846a210167904576cdf3d972
codiaji <codiaji@gmail.com>
2023-05-05 11:03:03 +0100

    allow at most one click per second, with rxjs- my version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-let count = 0;
-let rate = 1000;
-let lastClick = Date.now() - rate;
-document.addEventListener("click", () => {
-  if (Date.now() - lastClick >= rate) {
-    console.log(`Clicked ${++count} times`);
-    lastClick = Date.now();
-  }
-});
+import { fromEvent, scan, debounce } from "rxjs";
+
+fromEvent(document, "click")
+  .pipe(scan((count) => count + 1, 0))
+  .pipe(
+    debounce((count) => {
+      setTimeout(() => {
+        count;
+      }, 1000);
+    })
+  )
+  .subscribe((count) => console.log(`Clicked ${count} times`));
